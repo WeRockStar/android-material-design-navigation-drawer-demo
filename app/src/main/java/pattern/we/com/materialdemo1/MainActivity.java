c73b069a681eb7c709a8264c3379ba794f4afc18
comfreestyle <vr_freestyle@outlook.com>
2015-08-25 16:14:37 +0700

    Fixed Toolbar support actionbar

--- app/src/main/java/pattern/we/com/materialdemo1/MainActivity.java
+++ app/src/main/java/pattern/we/com/materialdemo1/MainActivity.java
@@ -2,15 +2,22 @@ package pattern.we.com.materialdemo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+
+        setSupportActionBar(toolbar);
     }
 
     @Override
