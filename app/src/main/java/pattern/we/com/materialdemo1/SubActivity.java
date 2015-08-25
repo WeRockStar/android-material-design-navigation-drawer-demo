275bbbcde0375e97043c796f3b088a8b0f0ce3dc
comfreestyle <vr_freestyle@outlook.com>
2015-08-25 16:34:44 +0700

    Create Sub Activity

--- app/src/main/java/pattern/we/com/materialdemo1/SubActivity.java
+++ app/src/main/java/pattern/we/com/materialdemo1/SubActivity.java
@@ -1,30 +1,22 @@
 package pattern.we.com.materialdemo1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sub);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -38,9 +30,6 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-        }
-        if (id == R.id.navigate) {
-            startActivity(new Intent(this, SubActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
